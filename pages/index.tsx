475ab773fcdbab13ecf8f3c15880509f465d44b5
Ha young, Kim <74491172+hayoung-jeremy@users.noreply.github.com>
2022-04-24 16:52:36 +0900

    chore : install tailwind css

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,11 @@
 import type { NextPage } from 'next';
 
 const Home: NextPage = () => {
-  return null;
+  return (
+    <div className='bg-[#333]'>
+      <h2 className='text-[#eee]'>Hello Next</h2>
+    </div>
+  );
 };
 
 export default Home;
