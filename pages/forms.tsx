828c09bd11177a937d6ef539adb1c9d2e0c998bb
Ha young, Kim <jeremiah91@naver.com>
2022-07-01 11:09:00 +0900

    feat : react-hook-form validation

--- pages/forms.tsx
+++ pages/forms.tsx
@@ -1,14 +1,55 @@
 import React from "react"
-import { useForm } from "react-hook-form"
+import { FieldErrors, useForm } from "react-hook-form"
+
+interface LoginForm {
+  username: string
+  password: string
+  email: string
+}
 
 const Forms = () => {
-  const { register, watch } = useForm()
+  const { register, watch, handleSubmit } = useForm<LoginForm>()
+  const onValid = (data: LoginForm) => {
+    console.log(data)
+  }
+  const onInvalid = (errors: FieldErrors) => {
+    console.log(errors)
+  }
   return (
     <div className="">
-      <form className="p-5 w-full flex flex-col gap-3">
-        <input {...register("username")} type="text" placeholder="name" />
-        <input {...register("email")} type="text" placeholder="email" />
-        <input {...register("password")} type="text" placeholder="password" />
+      <form
+        className="p-5 w-full flex flex-col gap-3"
+        onSubmit={handleSubmit(onValid, onInvalid)}
+      >
+        <input
+          {...register("username", {
+            required: "Username is required",
+            minLength: {
+              message: "The username should be longer than 5 characters.",
+              value: 5,
+            },
+          })}
+          type="text"
+          placeholder="name"
+          className="bg-[#3f3b36] text-[#eee]"
+        />
+        <input
+          {...register("email", { required: "Email is required" })}
+          type="text"
+          placeholder="email"
+          className="bg-[#3f3b36] text-[#eee]"
+        />
+        <input
+          {...register("password", { required: "Password is required" })}
+          type="text"
+          placeholder="password"
+          className="bg-[#3f3b36] text-[#eee]"
+        />
+        <input
+          type="submit"
+          value="Create Account"
+          className="bg-[#3f3b36] text-[#eee] py-3"
+        />
       </form>
     </div>
   )
