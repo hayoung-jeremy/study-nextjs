6278c3b41c399a1fcf8705ba53412f0a4afa64b7
Ha young, Kim <jeremiah91@naver.com>
2022-07-07 10:12:40 +0900

    feat : next API, withHandler

---
+++ libs/server/withHandler.ts
@@ -0,0 +1,22 @@
+import { NextApiRequest, NextApiResponse } from "next"
+
+type methods = "GET" | "POST" | "DELETE"
+
+const withHandler = (
+  method: methods,
+  fn: (req: NextApiRequest, res: NextApiResponse) => void
+) => {
+  return async function (req: NextApiRequest, res: NextApiResponse) {
+    if (req.method !== method) {
+      return res.status(405).end()
+    }
+    try {
+      await fn(req, res)
+    } catch (error) {
+      console.log(error)
+      return res.status(500).json({ error })
+    }
+  }
+}
+
+export default withHandler
