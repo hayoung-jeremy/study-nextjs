2e20155ff4aed28b8de348c16364d7ca2d99d6a0
Ha young, Kim <jeremiah91@naver.com>
2022-07-07 10:55:19 +0900

    feat : Authentication / create user with email, phone

--- pages/api/users/enter.tsx
+++ pages/api/users/enter.tsx
@@ -1,8 +1,51 @@
 import { NextApiRequest, NextApiResponse } from "next"
 import withHandler from "@libs/server/withHandler"
+import prismaClient from "@libs/server/client"
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
-  console.log(req.body)
+  const { email, phone } = req.body
+  let user
+
+  if (email) {
+    user = await prismaClient.user.findUnique({
+      where: {
+        email,
+      },
+    })
+    if (user) console.log("Already exist, please log in")
+    if (!user) {
+      console.log("Did not find any user, will create one")
+      // create User
+      user = await prismaClient.user.create({
+        data: {
+          name: "Anonymous",
+          email,
+        },
+      })
+    }
+    // console.log("user : ", user)
+  }
+
+  if (phone) {
+    user = await prismaClient.user.findUnique({
+      where: {
+        phone: +phone,
+      },
+    })
+    if (user) console.log("Already exist, please log in")
+    if (!user) {
+      console.log("Did not find any user, will create one")
+      // create User
+      user = await prismaClient.user.create({
+        data: {
+          name: "Anonymous",
+          phone: +phone,
+        },
+      })
+    }
+    // console.log("user : ", user)
+  }
+  // console.log(req.body)
   return res.status(200).end()
 }
 
